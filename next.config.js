91b3404495b47d7ea8ea7180aaf79ea55f36e68e
Matthew Nickson <mnickson@sidingsmedia.com>
2022-06-06 19:17:36 +0100

    Created initial next boiler plate app
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2022 Vercel, Inc.
+// SPDX-License-Identifier: MIT
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
