f7ddd746e69efbfece0b5314da8169fe8df2c280
Matthew Nickson <mnickson@sidingsmedia.com>
2023-04-09 11:40:36 +0100

    Removed old files
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

--- next.config.js
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2022 Vercel, Inc.
-// SPDX-License-Identifier: MIT
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
